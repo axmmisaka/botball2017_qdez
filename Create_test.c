113a1b6db0b27220fa0653690d733b97fbabf6e0
Frank X. Blackburn <axmmisaka@users.noreply.github.com>
2017-03-04 10:54:29 +0800

    Update Create_test.c

--- Create_test.c
+++ Create_test.c
@@ -1,8 +1,33 @@
 //coded by axmmisaka
-int CreateConnect(){
-while(create_connect()) {
-		msleep(400);
-	}
- console_clear();
+#include "kipr/botball.h"
+int CreateInit(){
+	while(create_connect()) {
+			msleep(400);
+		}
+	console_clear();
+	create_full();
+	return EXIT_SUCCESS;
 }
 
+int main(void){
+	CreateInit();
+	printf("Press Play button to continue.\n");
+	printf("Battery is now %d mAh with a full of %d mAh",get_create_battery_charge(),get_create_battery_capacity());
+	while(!get_create_play_button());
+	console_clear();
+	printf("Moving and Sensor test");
+	while(1){
+	create_drive_strait();
+		if(get_create_rbump()){
+			create_stop();
+			set_create_normalized_angle(0);
+			set_create_total_angle(0);
+			create_spin_CW(20);
+				while(set_create_normalized_angle()<=90);
+			create_stop();
+		}
+	if(get_create_play_button())
+		break;
+	}
+}
+	
