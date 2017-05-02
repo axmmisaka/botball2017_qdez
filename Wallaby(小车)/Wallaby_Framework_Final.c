0ca75fb915d33c9ea2841587ea91939ccd63b8d1
Frank X. Blackburn <axmmisaka@users.noreply.github.com>
2017-05-02 22:01:32 +0800

    Create Wallaby_Framework_Final.c

---
+++ Wallaby(小车)/Wallaby_Framework_Final.c
@@ -0,0 +1,9 @@
+#include <kipr/botball.h>
+
+int PIDLineFollow();
+int HitBigBlueBall();
+int HitSmallBlueBall();//servo 1024
+int goBack
+int setServoInitPos();//servo 350
+int goline();
+//when the hand goes across the servo,   close it
