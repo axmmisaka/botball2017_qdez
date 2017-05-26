625a88dcfc1625aecf64a97ad51acf09fb2e35dd
Frank X. Blackburn <axmmisaka@users.noreply.github.com>
2017-05-26 20:39:27 +0800

    Updated to program used in national qualifier
    Finished by axmmisaka and Charlie5381.
    Used in all competitions in national qualifier.

--- Wallaby(小车)/main_framework.c
+++ Wallaby(小车)/main_framework.c
@@ -1,16 +1,3 @@
-/*************************************************************/
-/*Some before-running initializations*/
-/*Connections:Left wheel on Motor 0, with red side towards the screen.*/
-/*Right wheel on Motor 1, with black side towards the screen.*/
-/*The nearer servo, mounted onto the upright post is connected on servo 0*/
-/*The another servo, servo 1 which controls the open and closeof mechanican hand, is connected on servo 2*/
-/*Tophat sensor is mounted on analog 0, light sensor is mounted on analog 1.*/
-/*The back wheel has a distance of 17.5 centimeter from the outer border of the starting area horizontally, 8.5 cm vertically*/
-/*************************************************************/
-
-
-
-
 //Finished by Charlie5381 and axmmisaka
 #define SERVO_LIFT 0
 #define SERVO_CATCH 1
@@ -19,10 +6,35 @@
 const int initialMotorPower=75;
 float motorPowerDifference=0;
 int motorPowerAlternateRatio=0;
-int offset=(3850+1750)/2;//offset = Tophat detected white + black /2
+int offset=(3800+1500)/2;//offset = Tophat detected white + black /2
 float elapsedTime;
-float AdjustmentRatio=80.0/2100.0;/*(100-0)/(3900-2100)*///AdjustmentRatio = (MotorMax-MotorMin)/(Tophat detected black - white)
+float AdjustmentRatio=100.0/2300.0;/*(100-0)/(3900-2100)*///AdjustmentRatio = (MotorMax-MotorMin)/(Tophat detected black - white)
 int currentValue;
+void check()
+{
+
+	int i=200;
+  while (i<=1900)
+    {
+    	set_servo_position(0,i);
+      	i+=1;
+      	msleep(1);
+  	}
+  set_servo_position(1,300);
+  set_servo_position(1,1200);
+   set_servo_position(1,300);
+  msleep(500);
+    set_servo_position(1,1200);
+  msleep(500);
+    set_servo_position(1,300);
+  msleep(500);
+    while (i>=200)
+    {
+    	set_servo_position(0,i);
+      	i-=1;
+      	msleep(1);
+  	}
+}
 void PID_lineFollow()
 {
   motorPowerAlternateRatio=currentValue-offset;
@@ -30,99 +42,230 @@ void PID_lineFollow()
   printf("(DEBUG)%f %f %d\n",motorPowerDifference,AdjustmentRatio,motorPowerAlternateRatio);
   printf("(DEBUG)%f %f\n",initialMotorPower-motorPowerDifference,initialMotorPower+motorPowerDifference);
   motor(0,initialMotorPower+motorPowerDifference);
+    motor(2,initialMotorPower+motorPowerDifference);
   motor(1,initialMotorPower-motorPowerDifference);
+    motor(4,initialMotorPower-motorPowerDifference);
 }
 void initialization()
 {
-  enable_servo(SERVO_LIFT);
-  enable_servo(SERVO_CATCH);
-  set_servo_position(SERVO_LIFT,650);
-  set_servo_position(SERVO_CATCH,1200);
+  set_servo_position(0,200);
+  set_servo_position(1,1200);
+}
+void pid()
+{
+	motorPowerAlternateRatio=currentValue-2250;
+ 	motorPowerDifference=(100.0/(3800.0-700.0))*motorPowerAlternateRatio;
+ 	printf("(DEBUG)%f %f %d\n",motorPowerDifference,AdjustmentRatio,motorPowerAlternateRatio);
+ 	printf("(DEBUG)%f %f\n",70-motorPowerDifference,70+motorPowerDifference);
+  motor(0,initialMotorPower+motorPowerDifference);
+    motor(2,initialMotorPower+motorPowerDifference);
+  motor(1,initialMotorPower-motorPowerDifference);
+    motor(4,initialMotorPower-motorPowerDifference);
 }
 void ascent()
 {
-  elapsedTime=seconds()+2;
+  set_servo_position(SERVO_LIFT,300);
+  msleep(1000);
+  motor(0,70);
+  motor(1,70);
+  msleep(200);
+  motor(0,0);
+  motor(1,0);
+  int i=1200;
+  while (i>=200)
+    {
+    	set_servo_position(1,i);
+      	i-=1;
+      	msleep(1);
+  	}
+  msleep(1000);
+  motor(0,-70);
+  motor(1,-70);
+  msleep(200);
+  motor(0,0);
+  motor(1,0);
+  set_servo_position(0,1000);
+  msleep(1000);
+  motor(0,70);
+  motor(1,70);
+  msleep(200);
+  motor(0,0);
+  motor(1,0);
+  msleep(1000);
+  
+  elapsedTime=seconds()+1.5;
   printf("(DEBUG)%f  %f  %f",elapsedTime,seconds(),elapsedTime-seconds());
   while(seconds()<elapsedTime)
   {
     motor(0,70);
     motor(1,70);
   }
-  disable_servo(SERVO_LIFT);
+  motor(0,1);
+  motor(1,1);
+  set_servo_position(0,200);
+  msleep(1000);
   currentValue=analog(0);
-  elapsedTime=seconds()+5.1;
+  elapsedTime=seconds()+4.4;
   while (seconds()<elapsedTime)
   {
     printf("(debug)%f\n",elapsedTime-seconds());
     currentValue=analog(0);
     PID_lineFollow();
   }
-}
-void moveOntoUpperArea()
-{
-  int servo_pos = 0;
-  enable_servo(SERVO_LIFT);
-  elapsedTime = seconds()+2;
-  while(seconds()<elapsedTime)
+  printf("shang\n");
+  //enable_servo(0);
+  motor(0,1);
+  motor(1,1);
+  set_servo_position(0,500);
+  msleep(1000);
+  elapsedTime = seconds()+0.7;
+  while (seconds()<elapsedTime )
   {
-    set_servo_position(SERVO_LIFT,servo_pos = servo_pos < 400 ? servo_pos + 5:400);
-    motor(0,80);
+  	motor(0,80);
     motor(1,80);
   }
-  disable_servo(SERVO_LIFT);
-  PID_lineFollow();
-}
-void ballCollect()
-{
-  set_servo_position(SERVO_LIFT,500);
-  elapsedTime+=1;
-  while (seconds()<elapsedTime)
-  {
-    motor(0,initialMotorPower);
-    motor(1,initialMotorPower);
-  }
-  elapsedTime+=1;
-  while (seconds()<elapsedTime)
-  {
-    currentValue=analog(0);
-    PID_lineFollow();
-  }
+   motor(0,1);
+  motor(1,1);
+  msleep(2000);
+  motor(0,70);
+  motor(1,70);
+  msleep(1000);
   motor(0,0);
-  elapsedTime+=3;
-  int i=1200;
+  motor(1,0);
+  msleep(1000);
+    set_servo_position(0,200);
+  msleep(1000);
+  set_servo_position(1,1200);
+  msleep(1000);
+  motor(0,-10);
+  motor(1,-10);
+  i=200;
+  while (i<=450)
+  {
+    set_servo_position(0,i);
+    i+=1;
+  msleep(10);
+  }
+  msleep(1500);
+//  motor(0,-70);
+//  motor(1,-70);
+//  msleep(1000);
+  motor(0,0);
+  motor(1,0);
+  msleep(1000);
+      set_servo_position(0,200);
+  msleep(1000);
+  elapsedTime = seconds()+1;
   while (seconds()<elapsedTime)
   {
-    motor(0,-10);
-    motor(1,-10);
-    while (i>530)
-    {
-      i-=10;
-      set_servo_position(0,i);
-      msleep(10);
-    }
+  	motor(0,80);
+    motor(1,80);
+  }
+//    motor(0,1);
+ // motor(1,1);
+  //msleep(2000);
+  elapsedTime = seconds()+8;
+  while (seconds()<elapsedTime && (analog(2) || elapsedTime-seconds()>4.5))
+  {  
+    currentValue=analog(0);
+    pid();
+    printf("%f\n",elapsedTime-seconds());
   }
   motor(0,0);
   motor(1,0);
-  i=500;
-  while (i<2000)
+  msleep(2000);
+}
+void ballCollect()
+{
+  //set_servo_position(SERVO_LIFT,500);
+  elapsedTime=seconds()+10;
+  int i=1200;
+  while (seconds()<elapsedTime)
   {
-    i+=10;
-    set_servo_position(SERVO_LIFT,i);
-    msleep(10);
+    
+    motor(0,-10);
+    motor(1,-10);
+    msleep(1500);
+    while (i>=200)
+    {
+    	set_servo_position(1,i);
+      	i-=1;
+      	msleep(1);
+  	}
   }
-  motor(0,initialMotorPower);
-  motor(1,initialMotorPower);
-  msleep(100);
-  set_servo_position(SERVO_LIFT,1800);
-  set_servo_position(SERVO_CATCH,1000);
-
+  i=200;
+  while (i<=2000)
+  {
+  	set_servo_position(0,i);
+    i+=1;
+    msleep(1);
+  }
+  elapsedTime=seconds()+2.5;
+  while (seconds()<elapsedTime)
+  {
+    motor(0,100);
+    motor(1,70);
+  //	currentValue=analog(0);
+  //  pid();
+  }
+  motor(0,-70);
+  motor(1,-100);
+  msleep(1500);
+  motor(0,100);
+  motor(1,100);
+  msleep(1500);
+  motor(0,0);
+  motor(1,0);
+  set_servo_position(0,1700);
+  msleep(1000);
+  set_servo_position(1,1200);
+  msleep(2000);
+    set_servo_position(0,1900);
+  motor(0,-30);
+  motor(1,-30);
+  msleep(2000);
+  motor(0,30);
+  motor(1,30);
+  msleep(2000);
+  motor(0,0);
+  motor(1,0);
+  msleep(1000);
+  set_servo_position(0,1700);
+   set_servo_position(1,300);
+  msleep(500);
+    set_servo_position(1,1200);
+  msleep(500);
+    set_servo_position(1,300);
+  msleep(500);
+    set_servo_position(1,1200);
+  msleep(500);
+    set_servo_position(1,300);
+  msleep(500);
+    set_servo_position(1,1200);
+  msleep(500);
+    set_servo_position(1,300);
+  msleep(500);
+    set_servo_position(1,1200);
+  msleep(500);
 }
 int main()
 {
+      enable_servo(SERVO_LIFT);
+  enable_servo(SERVO_CATCH);
+ // check();
   initialization();
-  printf("initialization success...\nBoiling Vodka!\n");
-  ascent();
-  moveOntoUpperArea();
-  //ballCollect();
+  printf("READY...\n");
+  while (1)
+  {
+    if (analog(1)<=200)
+    { 
+      printf("20s left to begin\n");
+      msleep(20000);
+   	 ascent();
+		// moveOntoUpperArea();
+ 		 ballCollect();
+    break;
+  	}
+  }
   return 0;
 }
