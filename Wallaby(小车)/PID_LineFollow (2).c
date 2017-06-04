e8e3e49ca3d51b7308c69d09288a25902928bf0b
axmmisaka <985379990@qq.com>
2017-06-05 00:45:22 +0800

    Fixed PID_linefollow
    1.added todo list
    2.changed names or varibles
    3.used #define to indicate motors
    4.fixed wrong timing method
    5.added comments, there used to be no comments
    Peace!

--- Wallaby(小车)/PID_LineFollow (2).c
+++ Wallaby(小车)/PID_LineFollow (2).c
@@ -1,38 +1,47 @@
+//PID Algorithm is an algorithm that could make the robot follow the line without huge swing.
+//This program can let the wallaby follow the black line and climb the ramp
+//Finished by Charlie5381 and axmmisaka
+//Variables names and some details are changed by axmmisaka
+
 #include <kipr/botball.h>
-const int power=50;
-int cz=0;
-int er=0;
-int pc=(2100+3900)/2;
-float t;
-float kp=0.05;
-int dh;
-void pid()
+#define MOTOR_LEFT 0
+#define MOTOR_RIGHT 1
+const int MotorPower=50;//TODO: try changing these globals vars into local
+int MotorPowerDifference=0;
+int MotorPowerAlternateRatio=0;
+int offset=(2100+3900)/2;//offset = Tophat detected white + black /2
+float AdjustmentRatio=0.05/*(100-0)/(3900-2100)*/;//AdjustmentRatio = (MotorMax-MotorMin)/(Tophat detected black - white)
+int CurrentValue;
+void LineFollow()//Will follow right edge of the BLACK duct tape
 {
-	er=dh-pc;
-  	cz=kp*er;
-  	motor(0,power-cz);motor(1,power+cz);
+	MotorPowerAlternateRatio = CurrentValue - offset;
+  	MotorPowerDifference = AdjustmentRatio * MotorPowerAlternateRatio;
+  	printf("%f\n",AdjustmentRatio);
+  	motor(MOTOR_LEFT, MotorPower - MotorPowerDifference);
+  	motor(MOTOR_RIGHT, MotorPower + MotorPowerDifference);
 }
 int main()
 {
-  	t=seconds()+3;
-  	while(seconds()<t)
+    float elapsedTime;
+  	elapsedTime=seconds()+3;
+  	while(seconds()<elapsedTime)
     {
-motor(0,70);
-motor(1,70);
+  		motor(MOTOR_LEFT,70);
+  		motor(MOTOR_RIGHT,70);
     }//Climb onto the ramp
-  	dh=analog(0);
-  	t+=10;
-  	while (seconds()<t)
+  	CurrentValue=analog(0);
+  	elapsedTime=seconds()+10;
+  	while (seconds()<elapsedTime)
     {
-  printf("%d\n",dh);
-   dh=analog(0);
-pid();
+      	printf("%d\n",CurrentValue);
+    	CurrentValue=analog(0);
+      	LineFollow();
     }
-  	t+=2;
-  	while (seconds()<t)
+  	elapsedTime=seconds()+2;
+  	while (seconds()<elapsedTime)
     {
-motor(0,power);
-      	motor(1,power);
-    }
+    	motor(0,MotorPower);
+      	motor(1,MotorPower);
+    }//get of the ramp, 2sec
     return 0;
 }
